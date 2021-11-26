1eaeea6d0f9bcfefbd4a63f7284ec0de93996f51
Demian-Wicky <demian.wicky@gmail.com>
2021-11-26 15:42:05 +0100

    q3 ok

--- script_modif_DOM.js
+++ script_modif_DOM.js
@@ -1,9 +1,9 @@
 // Modification n°1     changeTitles()
 function changeTitles(){
-    title = document.getElementsByClassName("jumbotron-heading")[0]
+    let title = document.getElementsByClassName("jumbotron-heading")[0]
     title.textContent = "Ce que j'ai appris à THP"
     /////
-    subtitle = document.getElementsByClassName("lead text-muted")[0]
+    let subtitle = document.getElementsByClassName("lead text-muted")[0]
     subtitle.textContent = "THP est une formation qui, en 3 mois, à plein temps, vous apportera des connaissances actionnables pour vous permettre de voir plus loin. Chez nous, pas de professeurs, pas de locaux, mais un groupe de travail en présentiel. Après 11 semaines, les principaux langages et outils du web n'auront plus de secret pour vous !"
 }
 changeTitles()
@@ -11,11 +11,11 @@ changeTitles()
 
 // Modification n°2     changeCallToActions()
 function changeCallToActions(){
-    btn1 = document.getElementsByClassName("jumbotron")[0].getElementsByClassName("btn-primary")[0]
+    let btn1 = document.getElementsByClassName("jumbotron")[0].getElementsByClassName("btn-primary")[0]
     btn1.textContent = "OK je veux tester !"
     btn1.href = "http://www.thehackingproject.org"
     ///////
-    btn2 = document.getElementsByClassName("jumbotron")[0].getElementsByClassName("btn-secondary")[0]
+    let btn2 = document.getElementsByClassName("jumbotron")[0].getElementsByClassName("btn-secondary")[0]
     btn2.textContent = "Non Merci"
     btn2.href = "https://www.pole-emploi.fr/accueil/"
 }
@@ -23,7 +23,13 @@ changeCallToActions()
 
 
 // Modification n°3     changeLogoName()
-
+function changeLogoName(){
+    let title = document.getElementsByClassName("navbar")[0].getElementsByClassName("navbar-brand")[0].getElementsByTagName("strong")[0]
+    title.textContent = "The THP Experience (Non Merci)"
+    title.style.fontSize = "2em"
+    
+}
+changeLogoName()
 
 
 // Modification n°4     populateImages()
